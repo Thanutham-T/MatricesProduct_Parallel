2a0c7574936f99bd37134ce5525aaf08ac06d0f7
Thanutham Tuktian <thanutham.tn@gmail.com>
2024-12-29 10:01:54 +0700

    create MatrixBenchmarkPthread_01.cpp

---
+++ version_01/MatrixBenchmarkPthread_01.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+template<typename T>
+void generate_matrix_element(T** matrix, const size_t ROW, const size_t COL);
+
+template<typename T>
+void print_matrix(T** matrix, const size_t ROW, const size_t COL);
+
+template <typename T>
+T** allocate_matrix(const size_t ROW, const size_t COL);
+
+template <typename T>
+void deallocate_matrix(T** matrix, const size_t ROW);
+
+template<typename T>
+void create_operation_matrix(const size_t ROW, const size_t COL);
+
+int main() {
+    create_operation_matrix<double>(8, 8);
+    return 0;
+}
+
+template<typename T>
+void generate_matrix_element(T** matrix, const size_t ROW, const size_t COL) {
+    std::random_device rd;                          // Obtain a random number from hardware
+    std::mt19937 gen(rd());                         // Seed the generator
+    std::uniform_real_distribution<T> distr(0, 10);  // Define the range
+
+    for (size_t row = 0; row < ROW; ++row) {
+        for (size_t col = 0; col < COL; ++col) {
+            matrix[row][col] = distr(gen);
+        }
+    }
+}
+
+template<typename T>
+void print_matrix(T** matrix, const size_t ROW, const size_t COL) {
+    for (size_t row = 0; row < ROW; ++row) {
+        for (size_t col = 0; col < COL; ++col) {
+            std::cout << std::setw(3) << matrix[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T>
+T** allocate_matrix(const size_t ROW, const size_t COL) {
+    T** matrix = new T*[ROW];
+    for (size_t row = 0; row < ROW; ++row) {
+        matrix[row] = new T[COL];
+    }
+    return matrix;
+}
+
+template <typename T>
+void deallocate_matrix(T** matrix, const size_t ROW) {
+    for (size_t row = 0; row < ROW; ++row) {
+        delete[] matrix[row];  // Use delete[] for arrays
+    }
+    delete[] matrix;  // Use delete[] for array of pointers
+}
+
+template<typename T>
+void create_operation_matrix(const size_t ROW, const size_t COL) {
+    T** matrix_A = allocate_matrix<T>(ROW, COL);
+    T** matrix_B = allocate_matrix<T>(ROW, COL);
+    T** matrix_C = allocate_matrix<T>(ROW, COL);
+
+    std::cout << "Matrix A:" << std::endl;
+    generate_matrix_element(matrix_A, ROW, COL);
+    print_matrix(matrix_A, ROW, COL);
+
+    std::cout << "Matrix B:" << std::endl;
+    generate_matrix_element(matrix_B, ROW, COL);
+    print_matrix(matrix_B, ROW, COL);
+
+    std::cout << "Matrix C:" << std::endl;
+    print_matrix(matrix_C, ROW, COL);
+
+    deallocate_matrix(matrix_A, ROW);
+    deallocate_matrix(matrix_B, ROW);
+    deallocate_matrix(matrix_C, ROW);
+}
